48af83756822e959b778810a58fcaeba297d3f22
jlamaspi <jlamaspi@LIM-8JH59K3.usersad.everis.int>
2022-03-11 17:17:57 -0500

    Feat: 'project initialization'.

---
+++ public_service-cliente/src/main/java/com/bootcamp/public_servicecliente/PublicServiceClienteApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.public_servicecliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicServiceClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicServiceClienteApplication.class, args);
+	}
+
+}
---
+++ public_service-cliente/src/test/java/com/bootcamp/public_servicecliente/PublicServiceClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.public_servicecliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicServiceClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
